da8e3d2d10f829383691180bc0db16b82cf1c436
PENG Zhengshuai <zhengshuai.peng@outlook.com>
2021-12-05 16:02:59 +0800

    init github ci actions (#5)

---
+++ .github/scripts/ci_checking.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#npm install -g markdown-link-check
+
+# Check dead links
+check_deadlinks=true
+
+if [ "$check_deadlinks" == "true" ]; then
+    succ=1
+    function check_dead_link(){
+        for file in `ls $1`
+        do
+            if [ -d $1"/"$file ]; then
+                check_dead_link $1"/"$file $2
+            elif [ "${file: 0-3 :3}" == ".md" ]; then
+                markdown-link-check $1"/"$file -c check_dead_link_conf.json || succ=0
+            fi
+        done
+    }
+    echo "Start to check dead links."
+    check_dead_link kolo-lang
+    check_dead_link byzer-notebook
+    if [ $succ -eq 0 ]; then
+        echo "Found dead links, please find logs above."
+        exit 1
+    fi || exit 0;
+fi
+
